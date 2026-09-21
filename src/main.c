9ef62c232b29748530019cbd5258dcaa4681b7e8
NickW16 <171855220+NickW16@users.noreply.github.com>
2026-09-21 11:02:34 -0300

    Implement cd and pwd

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "shell.h"
 
@@ -9,7 +10,7 @@ int main(void) {
 	char *argv[MAX_ARGS + 1]; // array of word pointers
 
 	for (;;) {
-		fputs("shellzinha>", stdout); // print prompt
+		fputs("shellzinha> ", stdout); // print prompt
 		fflush(stdout);
 
 		if (!fgets(line, sizeof line, stdin)) {
@@ -23,11 +24,46 @@ int main(void) {
 		int argc = tokenize(line, argv);
 		if (argc == 0) continue;
 
+		// built-in commands:
 		if(strcmp(argv[0], "exit") == 0) break; // if user types "exit", quit
+		if(strcmp(argv[0], "pwd") == 0) {
+			char buf[1024];
 
-		for(int i = 0; i < argc; i++) {
-			printf(" arg[%d] = \"%s\"\n", i, argv[i]);
+			if(getcwd(buf, sizeof buf) == NULL) {
+				perror("pwd");
+				continue;
+			}
+			printf("%s\n", buf);
 		}
+
+		if(strcmp(argv[0], "cd") == 0) {
+			if(argc == 1) {
+				char *env_value = getenv("HOME");
+				if (env_value == NULL) { // bug fix incase home is not set
+					fprintf(stderr, "cd: HOME not set\n");
+					continue;
+				}
+				if (chdir(env_value) == 0) {
+					printf("Changed directory to %s\n", env_value);	
+					continue;
+				} else {
+					perror("cd failed");
+					continue;
+				}
+			} else {
+				if (chdir(argv[1]) == 0) {
+					continue;
+				} else { 
+					perror("cd failed");
+					continue;
+				}
+			}
+		}
+		
+		// for debug:
+		//	for(int i = 0; i < argc; i++) {
+		//		printf(" arg[%d] = \"%s\"\n", i, argv[i]);
+		//	}
 															 
 		run_command(argv);
 	}
--- src/parse.c
+++ src/parse.c
@@ -1,17 +1,16 @@
 #include "shell.h"
 
 int tokenize(char *line, char **argv) {
-	int argc = 0; // argument counts
-	char *p = line; 
+	int argc = 0;
+	char *p = line;
 
 	while (*p && argc < MAX_ARGS - 1) {
 		while (*p == ' ' || *p == '\t') p++;
 		if (!*p) break;
-		argv[argc++] = p; // increase count
+		argv[argc++] = p;
 		while (*p && *p != ' ' && *p != '\t') p++;
 		if (*p) *p++ = '\0';
 	}
 	argv[argc] = NULL;
 	return argc;
-
 }
