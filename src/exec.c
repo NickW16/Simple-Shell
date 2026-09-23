d3268c9e61ae4d80abd09cc1791c955694345282
NickW16 <171855220+NickW16@users.noreply.github.com>
2026-09-23 10:44:23 -0300

    Add < functionality and fix several bugs between < and > loops

--- src/exec.c
+++ src/exec.c
@@ -6,22 +6,37 @@
 #include <unistd.h>
 
 int run_command(char **argv) {
-	char *redirect_file = NULL;
-	int target_fd = -1;
+	// legacy
+	// char *redirect_file = NULL;
+	int out_open_flags = O_WRONLY | O_CREAT | O_TRUNC; // kernel flags for file handling
+	int in_open_flags = O_RDONLY; // kernel flags for file handling
+	
+	// this is for enabling < and > to be used at the same time
+	char *in_file = NULL;
+	char *out_file = NULL;
 
+	// scan for redirect
 	for (int i = 0; argv[i] != NULL; i++) {
 		if (strcmp(argv[i], ">") == 0) {
 			if (argv[i + 1] == NULL) { // error in redirect usage
 				perror("found redirect with no filename");
 				return -1;
 			}
-			redirect_file = argv[i + 1];
-			target_fd = STDOUT_FILENO; // dup
+			//redirect_file = argv[i + 1];
+			out_file = argv[i + 1];
 			argv[i] = NULL;
-			break;
+		} else if (strcmp(argv[i], "<") == 0) {
+			if (argv[i + 1] == NULL) {
+				perror("found redirect with no filename");
+				return -1;
+			}
+			//redirect_file = argv[i + 1];
+			in_file = argv[i + 1];
+			argv[i] = NULL;
 		}
 	}
 
+	// fork for command exec
 	pid_t pid = fork();
 	if (pid < 0) {
 		perror("fork");
@@ -29,13 +44,23 @@ int run_command(char **argv) {
 	}
 
 	if (pid == 0) {
-		if (redirect_file != NULL) {
-			int fd = open(redirect_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (in_file != NULL) {
+			int fd = open(in_file, in_open_flags, 0644);
 			if (fd == -1) {
 				perror("open");
 				_exit(1);
 			} else {
-				dup2(fd, target_fd);
+				dup2(fd, STDIN_FILENO);
+				close(fd);
+			}
+		}
+		if (out_file != NULL) {
+			int fd = open(out_file, out_open_flags, 0644);
+			if (fd == -1) {
+				perror("open");
+				_exit(1);
+			} else {
+				dup2(fd, STDOUT_FILENO);
 				close(fd);
 			}
 		}
